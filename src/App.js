f275acfbc39482431d139ba2881653a7904dd4c1
Efe Erhie <efe_erhie@git_commits.me>
2022-06-02 19:16:09 -0500

    updated server address to heroku

--- src/App.js
+++ src/App.js
@@ -74,9 +74,11 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
+
+
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://cryptic-beyond-55129.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -86,7 +88,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://cryptic-beyond-55129.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
